204db698221fc95b0bbce11af15755363d52b017
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-07-13 22:52:24 -0400

    route added

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const express = require("express");
 const PORT = process.env.PORT || 3000
 const app = express();
 
+require("./routes/htmlRoutes")(app)
+
 app.listen(PORT, () => console.log(`running on port ${PORT}`))
