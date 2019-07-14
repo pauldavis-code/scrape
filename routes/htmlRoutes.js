501c3efe9c20dd662afff0afd2e69e08a13de82a
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-07-13 23:36:57 -0400

    created axios route, hitting target and returning data

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,18 @@
+const axios = require("axios");
+const cheerio = require("cheerio")
+
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.json("hello!");
+    axios.get("https://www.billboard.com/charts/hot-100")
+      .then(function(response) {
+        const $ = cheerio.load(response.data);
+
+        $("div.chart-list")
+          .children("div.chart-list-item").each(function(i, element) {
+            // console.log($(this).children("div.chart-list-item__first-row").children("div.chart-list-item__text-wrapper").children("div.chart-list-item__text").children("div.chart-list-item__title").text())
+            console.log($(this).attr("data-artist"));
+            console.log($(this).attr("data-title"));
+          }); 
+      });
   });
 };
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// === EXPRESS
 const express = require("express");
 
 const PORT = process.env.PORT || 3000
@@ -6,3 +7,13 @@ const app = express();
 require("./routes/htmlRoutes")(app)
 
 app.listen(PORT, () => console.log(`running on port ${PORT}`))
+
+// === HANDLEBARS
+var exphbs = require("express-handlebars");
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//middleware to use static files - parameter is the file
+app.use(express.static("public"));
